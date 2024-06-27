12048fcf36c628b3c909d80de50b7a271fcb1b3d
naufal <naufal.razzan@hotmail.com>
2024-06-28 02:53:33 +0700

    remove VerifyJWT() middleware

--- pkg/middlewares/auth_middleware.go
+++ pkg/middlewares/auth_middleware.go
@@ -1,47 +1,18 @@
 package middlewares
 
 import (
-	"fmt"
 	"synapsis-backend-test/configs"
 
 	"github.com/gofiber/fiber/v2"
 	jwtware "github.com/gofiber/jwt/v3"
-	"github.com/golang-jwt/jwt/v5"
 )
 
-func AuthMiddleware() fiber.Handler{
+func AuthMiddleware() fiber.Handler {
 	return jwtware.New(jwtware.Config{
-		SigningKey: []byte(configs.GetConfig().JWTSecretKey),
+		SigningKey:    []byte(configs.GetConfig().JWTSecretKey),
+		SigningMethod: "HS256",
+		ErrorHandler: func(c *fiber.Ctx, err error) error {
+			return fiber.NewError(fiber.StatusUnauthorized, err.Error())
+		},
 	})
-}
-
-func VerifyAuth() fiber.Handler{
-	return func(c *fiber.Ctx) error {
-		tokenString := c.Get("Authorization")
-		if tokenString == ""{
-			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-				"message": "missing auth header",
-			})
-		}
-
-		token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
-			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok{
-				return nil, fmt.Errorf("unexpected signin method: %v", t.Method)
-			}
-
-			return []byte(configs.GetConfig().JWTSecretKey), nil
-		})
-
-		if err != nil || !token.Valid{
-			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-				"message": "invalid token",
-			})
-		}
-
-		claims := token.Claims.(jwt.MapClaims)
-
-		c.Locals("user_id", claims["user_id"].(string))
-
-		return c.Next()
-	}
 }
