476438ac86719f30c68bf38bbf53cbdf4f6fed26
naufal <naufal.razzan@hotmail.com>
2024-06-28 02:54:08 +0700

    add routes on the whitelists

--- pkg/middlewares/undefined_routes.go
+++ pkg/middlewares/undefined_routes.go
@@ -2,26 +2,32 @@ package middlewares
 
 import "github.com/gofiber/fiber/v2"
 
-func UndefinedRoutesMiddleware() fiber.Handler{
+func UndefinedRoutesMiddleware() fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		allowedpath := []string{
-			"",
+			"/register",
+			"/login",
+			"/v1/listProduct/",
+			"/v1/insertShoppingCart",
+			"/v1/listShoppingCart",
+			"/v1/deleteShoppingCart",
+			"/v1/checkout",
 		}
 
 		matchedPath := false
-		for _, testpath := range allowedpath{
-			if c.Path() == "/"{
+		for _, testpath := range allowedpath {
+			if c.Path() == "/" {
 				return c.Status(fiber.StatusOK).JSON(fiber.Map{
 					"message": "application running smoothly",
 				})
 			}
-			if c.Path() == testpath{
+			if c.Path() == testpath {
 				matchedPath = true
 				break
 			}
 		}
 
-		if !matchedPath{
+		if !matchedPath {
 			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
 				"message": "resource not found",
 			})
